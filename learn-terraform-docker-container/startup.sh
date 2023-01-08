fa69697692f4523d998f4701be3be1e4a33fe7c4
Oskar Åsbrink <asbrinkoskar@gmail.com>
2023-01-08 15:20:59 +0100

    now doing the whole shebang

--- learn-terraform-docker-container/startup.sh
+++ learn-terraform-docker-container/startup.sh
@@ -34,9 +34,12 @@ cd GIT/learning-tf-delta-kafka
 sudo docker build -t spark3x-delta .
 
 #chmod +x *.sh
+chmod +x /home/ec2-user/GIT/learning-tf-delta-kafka/learn-terraform-docker-container/docker-start.sh
 ls -al /home/ec2-user
+ls -al /home/ec2-user/GIT/learning-tf-delta-kafka
+ls -al /home/ec2-user/GIT/learning-tf-delta-kafka/learn-terraform-docker-container
 pwd
-chmod +x /home/ec2-user/GIT/exjobb/learning-tf-delta-kafka/learn-terraform-docker-container/docker-start.sh
-sudo docker run --rm -i --name=spark3x-delta --mount type=bind,source=/home/ec2-user/GIT,destination=/root/GIT -p 4040:4040 spark3x-delta /root/GIT/exjobb/learning-tf-delta-kafka/learn-terraform-docker-container/docker-start.sh
+
+sudo docker run --rm -i --name=spark3x-delta --mount type=bind,source=/home/ec2-user,destination=/root -p 4040:4040 spark3x-delta /root/docker-start.sh
 #sudo docker run --rm -d -it --name=spark3x-delta --mount type=bind,source=/home/ec2-user/GIT,destination=/root/GIT -p 4040:4040 spark3x-delta /bin/bash /home/ec2-user/docker-start.sh
 #docker run --rm  -i --name=haskell-pinot --env-file $SCRIPTS_DIR/env.list -v $GITHUB_TEMPMDBOOKDIR/dbc/scalable-data-science:/root/temp --mount type=bind,source=$GITHUB_DIR_TO_REPO,destination=/root/GIT lamastex/haskell-pinot:latest /bin/bash $localscriptDIRPATH/pinotMdBook.sh
--- lib/src/main/scala/Main.scala
+++ lib/src/main/scala/Main.scala
@@ -1,14 +1,25 @@
+import scala.collection.immutable._
 object Main extends App {
+  def getCredentials(): List[String] = {
+    val filename = "/root/cred/aws_credentials.txt"
+    val lines = scala.io.Source.fromFile(filename).getLines.toList
+    val pub_key : String = lines(1).split(" ").last
+    val priv_key : String = lines(2).split(" ").last
+    return List(pub_key,priv_key)
+  }
+
   import org.apache.spark.sql.SparkSession
   import io.delta.tables.DeltaTable
+  val tokens: List[String] = getCredentials()
+
   val spark = SparkSession
       .builder()
       .appName("Utilities")
       .master("local[*]")
       .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
       .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-      .config("spark.hadoop.fs.s3a.access.key","asd123")
-      .config("spark.hadoop.fs.s3a.secret.key","asd123")
+      .config("spark.hadoop.fs.s3a.access.key",tokens(0))
+      .config("spark.hadoop.fs.s3a.secret.key",tokens(1))
 
       .getOrCreate()
 
